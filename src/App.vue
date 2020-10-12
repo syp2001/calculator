368ec0e5a2102a728d9e586badb66a7e8d3b206f
syp2001 <seyongpark2001@gmail.com>
2020-10-11 21:59:15 -0400

    implemented basic calculator

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <template>
   <div id="app">
-    <Header />
-    <Block v-for="block in blocks" :key="block.id" :data="block"/>
+    <h1>Scientific Calculator</h1>
+    <input v-model="expression" id="input" placeholder="type expression here"/>
+    <h1 v-if="output" id="display">= {{output}}</h1>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import Block from './components/Block.vue';
-import Header from './components/Header.vue';
+import { Component, Vue, Watch } from 'vue-property-decorator';
+import { evaluate } from 'mathjs';
 
 @Component({
   components: {
-    Block,
-    Header
   },
 })
 export default class App extends Vue {
-  public blocks: object[] = [{value:0,units:'m'}];
+  public output: string | undefined;
+  public expression = "";
 
+  @Watch('expression')
+  inputChanged(newVal: string) {
+    try{
+      this.output = evaluate(newVal);
+    }
+    catch(e){
+      this.output = "invalid input";
+    }
+  }
 }
 </script>
 
@@ -29,9 +37,41 @@ export default class App extends Vue {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
 }
 body {
   background-color: var(--light-blue);
 }
+h1 {
+  color:white;
+  text-align:left;
+  margin-left:20px;
+  margin-top:0;
+}
+#display {
+  color:white;
+  text-align: center;
+  font-weight:100;
+}
+#input {
+	margin: 20px auto;
+	outline: none;
+	border: none;
+	background-color: var(--blue);
+	border-radius: 3px;
+	color: white;
+	padding: 5px 10px;
+	transition: box-shadow 100ms 0ms ease-in;
+  font-size: 2em;
+  width: 50%;
+}
+#input:focus {
+	box-shadow: 0px 5px var(--lemon);
+	background-color: var(--light-blue);
+}
+
+body:focus-within {
+	/*background-color: var(--blue);*/
+}
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue'
 import './registerServiceWorker'
 import './assets/tailwind.css'
 import './assets/colors.css'
-import './postcss.config.ts'
 
 Vue.config.productionTip = false
 
