a1fbfcfbd539220e986a1df4656b8a16d724e563
syp2001 <seyongpark2001@gmail.com>
2020-10-12 21:45:12 -0400

    animations and buttons

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,42 @@
 <template>
   <div id="app">
-    <h1>Scientific Calculator</h1>
-    <input v-model="expression" id="input" placeholder="type expression here"/>
-    <h1 v-if="output" id="display">= {{output}}</h1>
+    <h1 class="floatup">Chemistry Calculator</h1>
+    <div class="container">
+      <input v-model="expression" class="floatup" id="input" placeholder="type expression here"/>
+      <Button @click="clear()" class="floatup clr">Clear</Button>
+    </div>
+    <h1 :class="output?'':'hide'" id="display">= {{output}}</h1>
+    <div class="container">
+    <Button v-for="constant in constants" :key="constant[0]" @click="append(constant[1])" class="floatup">
+      {{constant[0]}}
+    </Button>
   </div>
+  </div>
+ 
 </template>
 
 <script lang="ts">
 import { Component, Vue, Watch } from 'vue-property-decorator';
 import { evaluate } from 'mathjs';
+import Button from './components/Button.vue';
+import {constants} from './assets/constants.ts';
 
 @Component({
   components: {
+    Button
   },
 })
 export default class App extends Vue {
   public output: string | undefined;
   public expression = "";
+  public constants = constants;
 
+  public clear(): void{
+    this.expression = "";
+  }
+  public append(num: string): void{
+    this.expression += num;
+  }
   @Watch('expression')
   inputChanged(newVal: string) {
     try{
@@ -39,8 +58,17 @@ export default class App extends Vue {
   color: #2c3e50;
   margin-top: 10px;
 }
+.hide{
+  opacity:0;
+}
 body {
   background-color: var(--light-blue);
+  transition: all 500ms ease-in;
+  text-align: center;
+}
+.container {
+  /*width: 840px;*/
+  margin: 10px auto;
 }
 h1 {
   color: #e1f8ff;
@@ -48,6 +76,9 @@ h1 {
   margin-left:20px;
   margin-top:0;
   font-weight:200;
+}
+.clr{
+  margin-left:-74px;
 }
 #display {
   color:white;
@@ -58,11 +89,12 @@ h1 {
 	margin: 20px auto;
 	outline: none;
 	border: none;
-	background-color: var(--light-blue);
+	background-color: transparent;
 	color: white;
 	padding: 5px 10px;
-	transition: box-shadow 150ms ease-in;
+	transition: box-shadow 200ms ease-in;
   font-size: 2em;
+  height:1.35em;
   width: 50%;
 	box-shadow: 0px 3px var(--blue);
   
@@ -72,7 +104,20 @@ h1 {
 }
 
 body:focus-within {
-	/*background-color: var(--blue);*/
+	background-color: var(--blue);
 }
-
+.floatup{
+  animation: float 500ms cubic-bezier(0.175, 0.885, 0.32, 1.275) 1s forwards;
+  opacity:0%;
+}
+@keyframes float {
+  0%{
+    transform:translate(0px,20px);
+    opacity:0%;
+  }
+  100%{
+    transform: translate(0,0px);
+    opacity:100%;
+  }
+}
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+    <button @click="$emit('click')" id="btn" >
+        <slot></slot>
+    </button>
+</template>
+<script lang="ts">
+import { Component, Vue, Watch } from 'vue-property-decorator';
+export default class Button extends Vue {
+
+}
+</script>
+<style scoped>
+#btn {
+    height:2em;
+    padding: 10px;
+    background-color: var(--blue);
+    color: white;
+    outline: none;
+    border: none;
+    transition: all 250ms ease-in 0s;
+    font-size: 1.5em;
+}
+#btn:hover {
+    background-color: var(--light-blue);
+}
+</style>
