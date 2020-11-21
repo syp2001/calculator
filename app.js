3a5f4702a25832df15f4cd4723e1a5b9d05aef95
syp2001 <seyongpark2001@gmail.com>
2020-11-20 21:17:40 -0500

    backend

---
+++ app.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+require('./crud');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(process.env.PORT || 3000, () => {console.log('listening on port ')+(process.env.PORT || 3000)});
