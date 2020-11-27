6d43b71dd44ab668c96ded40e08575bd160b2f8b
Seyong <seyongpark2001@gmail.com>
2020-11-27 14:39:59 -0500

    basic routes

--- routes/constants.js
+++ routes/constants.js
@@ -1,9 +1,31 @@
+const { constants } = require('buffer');
 const express = require('express');
+const crud = require('../crud');
 
-const router = express.Router();
+var router = express.Router();
 
-router.get('/', (req,res) => {
-    res.send({hello: "world"});
-});
+
+
+router.get('/top', async (req,res) => {
+    try{
+        const constant = await crud.topConstants();
+        res.send(constant);
+    }
+    catch(err){
+        console.error(err);
+        res.status(500)
+    }
+})
+
+router.get('/:id', async (req,res) => {
+    try{
+        const constant = await crud.getConstant(req.params.id);
+        res.send(constant);
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).send();
+    }
+})
 
 module.exports = router;
